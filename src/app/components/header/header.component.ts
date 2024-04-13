0d271462e0973a2d235e793b685a901e1750febd
Andre Souza <andresouzadev@gmail.com>
2024-04-13 18:33:20 -0300

    Refined CSS, added dark theme

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,43 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isDarkMode: boolean = false;
 
   toggleMenu() {
-    const menu = <HTMLElement>document.querySelector(".menu-links");
-    const icon = <HTMLElement>document.querySelector(".hamburger-icon");
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+
+    if (!menu) {
+      console.error("menu element not found");
+      return;
+    }
+
+    if (!icon) {
+      console.error("icon element not found");
+      return;
+    }
+
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+  }
+
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const body = document.getElementsByTagName('body')[0];
+    if (this.isDarkMode) {
+      body.classList.add('dark-mode');
+    } else {
+      body.classList.remove('dark-mode');
+    }
   }
 
 }
