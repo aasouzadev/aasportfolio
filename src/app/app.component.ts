0df6ab838dc1494f1e35b0505571f89962b9aacc
Andre Souza <andresouzadev@gmail.com>
2024-04-12 21:39:05 -0300

    start portfolio project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { ProfileComponent } from "./components/profile/profile.component";
+import { AboutComponent } from "./components/about/about.component";
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HeaderComponent, FooterComponent, ProfileComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent]
 })
 export class AppComponent {
-  title = 'aasportfolio';
+  title = 'AAS Portfolio';
 }
---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+openLinkedIn() {
+throw new Error('Method not implemented.');
+}
+openGithub() {
+throw new Error('Method not implemented.');
+}
+downloadCV() {
+throw new Error('Method not implemented.');
+}
+
+}
