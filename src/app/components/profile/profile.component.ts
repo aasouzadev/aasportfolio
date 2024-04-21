45b57b14b1036b633b4d47c6313372ffc90a5732
Andre Souza <andresouzadev@gmail.com>
2024-04-20 21:59:59 -0300

    add CV Download

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,17 +6,32 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
-openLinkedIn() {
-throw new Error('Method not implemented.');
-}
-openGithub() {
-throw new Error('Method not implemented.');
-}
-downloadCV() {
-throw new Error('Method not implemented.');
-}
+  openLinkedIn() {
+    throw new Error('Method not implemented.');
+  }
+  openGithub() {
+    throw new Error('Method not implemented.');
+  }
+  downloadCV() {
+    const pdfUrl = './../../../assets/AndreSouzaCV.pdf';
+    const fileName = 'AndreSouzaCV.pdf';
 
+    fetch(pdfUrl)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = fileName;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+      })
+      .catch((error) => {
+        console.error('Erro ao baixar o arquivo PDF', error);
+      });
+  }
 }
