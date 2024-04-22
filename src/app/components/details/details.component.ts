06fd8eb5fd636afff57f08c959004c25c7d50322
Andre Souza <andresouzadev@gmail.com>
2024-04-21 22:13:52 -0300

    add projects details and fix some scss parameters

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -24,7 +24,7 @@ export class DetailsComponent {
         'The Coletor Fácil, the ultimate solution for effective and secure inventory management! Designed to make the process easy, fast, and accessible.',
       features: [
         {
-          title: 'Inventory Balancing with Smart Barcode Recognition:',
+          title: 'Inventory Balancing with AI Barcode Recognition',
           expanded: false,
           description:
             'Our solution allows users to effortlessly create inventory balances by leveraging intelligent barcode recognition technology. This feature ensures swift and accurate inventory counts, empowering businesses to maintain optimal stock levels.',
@@ -87,6 +87,67 @@ export class DetailsComponent {
         'Nest.js',
       ],
       url: 'https://aasoftware.web.app/',
+    },
+    {
+      id: 3,
+      title: 'Desmembrador',
+      image: './assets/desmembradorPost.webp',
+      description:
+        'In the dynamic world of meat processing, accurate yield calculations are crucial for optimizing profits and ensuring efficient operations. Introducing the Desmembrador App, your one-stop solution for streamlining yield calculations, comparing results across different cuts and suppliers, and generating profit estimates based on margins and weight-based pricing.',
+      features: [
+        {
+          title: 'Yield Calculations',
+          expanded: false,
+          description: 'Simplify yield calculations for various meat cuts.',
+        },
+        {
+          title: 'Benchmarking and Analysis',
+          expanded: false,
+          description: 'Compare yield data across cuts and suppliers.',
+        },
+        {
+          title: 'Profit Estimation',
+          expanded: false,
+          description:
+            'Forecast potential earnings based on margin and weight-based pricing.',
+        },
+      ],
+      techs: [
+        'Angular',
+        'Ionic Framework',
+        'Firebase Auth, Firestore, Push Notifications',
+        'Still in development',
+      ],
+      url: 'https://aasoftware.web.app/products?app=desmembrador',
+    },
+    {
+      id: 4,
+      title:
+        'Crafted with Passion: A Portfolio of Clean Design, High Performance, and Angular 17 Excellence',
+      image: './assets/portfolio.webp',
+      description:
+        'Welcome to my portfolio, a testament to my unwavering passion for crafting digital experiences that are both visually captivating and functionally impeccable. As a skilled front-end developer, I meticulously blend clean design principles with cutting-edge technologies to deliver solutions that not only engage users but also drive business results.',
+      features: [
+        {
+          title: 'Achieving PageSpeed Excellence',
+          expanded: false,
+          description:
+            'Crafting high-performance websites is crucial for a successful portfolio. By adhering to best practices such as optimizing code structure, minimizing resource sizes, leveraging caching techniques, and ensuring efficient image loading, you can consistently achieve top PageSpeed Insights scores.',
+        },
+        {
+          title: 'Aesthetics and Functionality in Harmony',
+          expanded: false,
+          description:
+            'I believe that a website`s design should not merely be a superficial layer but an integral part of its functionality. My approach to design is deeply rooted in minimalism, ensuring that every element serves a purpose and contributes to an uncluttered, intuitive user experience. I meticulously craft HTML and CSS code, adhering to the latest web standards and best practices to create visually appealing and responsive interfaces that adapt seamlessly to any device.',
+        },
+        {
+          title: 'A Commitment to Excellence',
+          expanded: false,
+          description:
+            'My dedication to excellence extends beyond technical proficiency. I am committed to following industry best practices, ensuring that my code is well-documented, unit-tested, and optimized for performance. ',
+        },
+      ],
+      techs: ['HTML', 'CSS', 'Angular 17', 'Firebase Hosting'],
     },
   ];
 
